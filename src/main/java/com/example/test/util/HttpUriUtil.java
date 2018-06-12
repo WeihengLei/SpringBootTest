0e28d429a6d3171eb62ff958d7d4ac39ece50160
weiheng.lei <weiheng.lei@gtomato.com>
2018-06-12 12:50:36 +0800

    spring boot + add send e-mail util

--- src/main/java/com/example/test/util/HttpUriUtil.java
+++ src/main/java/com/example/test/util/HttpUriUtil.java
@@ -2,10 +2,24 @@ package com.example.test.util;
 
 import com.example.test.exceptions.BusinessException;
 import com.example.test.model.TestResponse;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.mime.HttpMultipartMode;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Map;
 
 public class HttpUriUtil {
 
@@ -35,5 +49,57 @@ public class HttpUriUtil {
             System.out.println("=====3"+testResponse.getResponseBody());
             //throw new BusinessException(testResponse.getRespCode(), testResponse.getMessage());
         }
+    }
+
+    public static byte[] watermarkPostHttp(String url, MultipartFile image, InputStream waterMarkInStream, Map<String, String> params) throws IOException {
+
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+
+        RequestConfig requestConfig = RequestConfig.custom().
+                setConnectTimeout(1000).setConnectionRequestTimeout(1000)
+                .setSocketTimeout(1000).setRedirectsEnabled(true).build();
+
+        HttpPost httpPost = new HttpPost(url);
+        //httpPost.setConfig(requestConfig);
+
+        String fileName = image.getOriginalFilename();
+        MultipartEntityBuilder builder = MultipartEntityBuilder.create();
+        builder.addBinaryBody("image",image.getInputStream(), ContentType.MULTIPART_FORM_DATA,fileName);
+        builder.addBinaryBody("wmimage",waterMarkInStream, ContentType.MULTIPART_FORM_DATA,"watermark.png");
+        builder.setMode(HttpMultipartMode.RFC6532);
+
+        if(params!=null){
+            for (Map.Entry<String, String> entry : params.entrySet()) {
+                builder.addTextBody(entry.getKey(), entry.getValue(), ContentType.TEXT_PLAIN.withCharset("UTF-8"));
+            }
+        }
+        byte[] bytes = null;
+        try {
+            org.apache.http.HttpEntity httpEntity = builder.build();
+            httpPost.setEntity(httpEntity);
+
+            long startTime = System.currentTimeMillis();
+            System.out.println("### RBB CALL watermarkAPI START ### use time ["+startTime+"] ms");
+            HttpResponse httpResponse = httpClient.execute(httpPost);
+            System.out.println("### RBB CALL watermarkAPI END ### use time ["+(System.currentTimeMillis()-startTime)+"] ms");
+
+            if(httpResponse != null){
+                System.out.println(httpResponse.getStatusLine().getStatusCode());
+                if (httpResponse.getStatusLine().getStatusCode() == 200) {
+                    bytes = EntityUtils.toByteArray(httpResponse.getEntity());
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                if(httpClient != null){
+                    httpClient.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return bytes;
     }
 }
--- src/test/java/test/UnitTest.java
+++ src/test/java/test/UnitTest.java
@@ -14,6 +14,8 @@ import com.example.test.entity.security.User;
 import com.example.test.entity.security.UserRole;
 import com.example.test.model.TestResponse;
 import com.example.test.service.TestService;
+import com.sun.javafx.tools.ant.DeployFXTask;
+import org.apache.commons.collections.map.HashedMap;
 import org.hamcrest.Matchers;
 import org.jasypt.encryption.StringEncryptor;
 import org.junit.Assert;
@@ -25,15 +27,24 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.ui.freemarker.FreeMarkerTemplateUtils;
+import org.springframework.ui.velocity.VelocityEngineUtils;
 import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
 
-import java.util.UUID;
-
+import javax.mail.internet.MimeMessage;
+import java.io.IOException;
+import java.util.Map;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
 import static org.junit.Assert.assertThat;
 
 
@@ -63,6 +74,13 @@ public class UnitTest {
 	@Autowired
 	private RoleRepository roleDao;
 
+
+	@Autowired
+	private JavaMailSender mailSender;
+	//发送邮件的模板引擎
+	@Autowired
+	private FreeMarkerConfigurer configurer;
+
 	@Before
 	public void setUp() {
 		mvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
@@ -86,44 +104,33 @@ public class UnitTest {
 	//@Ignore
 	public void test() throws Exception {
 
-		String uuid = UUID.randomUUID().toString();
 
-		User admin = new User();
-		User jack = new User();
 
-		admin.setUsername("admin_" + uuid);
-		admin.setPassword("admin");
+		MimeMessage mimeMessage = mailSender.createMimeMessage();
 
-		jack.setUsername("jack_" + uuid);
-		jack.setPassword("123456");
+		MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
+		helper.setFrom("1121008660@qq.com");
+		helper.setTo("1121008660@qq.com");
+		helper.setSubject("主题：祝福");
 
-		userDao.save(admin);
-		userDao.save(jack);
+		Map<String, Object> model = new HashedMap();
+		model.put("username1", "weiheng1");
+		model.put("username2", "weiheng2");
+		model.put("username3", "weiheng3");
 
-		Role adminRole = new Role();
-		Role userRole = new Role();
+		try {
+			Template template = configurer.getConfiguration().getTemplate("message.ftl");
+			try {
+				String text = FreeMarkerTemplateUtils.processTemplateIntoString(template, model);
 
-		adminRole.setRole("ROLE_ADMIN");
-		userRole.setRole("ROLE_USER");
-
-		roleDao.save(adminRole);
-		roleDao.save(userRole);
-
-		UserRole userRoleAdminRecord1 = new UserRole();
-		userRoleAdminRecord1.setUserId(admin.getId());
-		userRoleAdminRecord1.setRoleId(adminRole.getId());
-		userRoleDao.save(userRoleAdminRecord1);
-
-		UserRole userRoleAdminRecord2 = new UserRole();
-		userRoleAdminRecord2.setUserId(admin.getId());
-		userRoleAdminRecord2.setRoleId(userRole.getId());
-		userRoleDao.save(userRoleAdminRecord2);
-
-		UserRole userRoleJackRecord = new UserRole();
-		userRoleJackRecord.setUserId(jack.getId());
-		userRoleJackRecord.setRoleId(userRole.getId());
-		userRoleDao.save(userRoleJackRecord);
-
+				helper.setText(text, true);
+				mailSender.send(mimeMessage);
+			} catch (TemplateException e) {
+				e.printStackTrace();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
 	}
 
